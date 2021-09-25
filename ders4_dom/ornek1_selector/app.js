86f257245f705104c864042b6a142b9648d053c9
ragip2501 <ragip2501@gmail.com>
2021-09-25 18:09:16 +0300

    yeni commit

--- ders4_dom/ornek1_selector/app.js
+++ ders4_dom/ornek1_selector/app.js
@@ -1,7 +1,42 @@
 //================================================================ =====
-//                   GETELEMENTID
-
+//                   GETELEMENTBYID
+​
 const paragraf = document.getElementById("para").style;
 paragraf.backgroundColor = "black";
 paragraf.color = "white";
 paragraf.fontSize = "20px";
+​
+​
+const buton=document.getElementById("btn");
+buton.style.width="100px";
+buton.style.color="red";
+buton.style.fontSize="18px";
+buton.textContent="ARA"; //textContent=tag lerin içeriğindeki yazıyı değiştirmek istersek
+​
+​
+//***** GETELEMENTBYTAGNAME */
+​
+const resim=document.getElementsByTagName("img");
+​
+resim[0].style.width="300px";
+resim[0].style.height="300px";
+resim[1].style.border="3px solid red";
+​
+//*****GETELEMENTBYCLASSNAME */
+​
+​
+const baslık=document.getElementsByClassName("h1");
+​
+baslık[0].style.textAlign="center";
+
+//==========QUERYSELECTOR===========
+
+//! ençok kullanılan çağırma yolu
+
+document.querySelector(".title").
+document.querySelector(".arama").textContent="DOM SELECTOR KAVRAMI😄";
+document.querySelector("#govde").style.backgroundImage="linear-gradient(green, blue, pink)";
+
+
+//*************EVENT********************
+
